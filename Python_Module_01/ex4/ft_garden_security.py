420df70468cc688f3103c560eacd341e6a926540
Jamielly Reis <100716307+Jamielly@users.noreply.github.com>
2026-08-08 15:43:39 -0300

    update mod01 0 e 4

--- Python_Module_01/ex4/ft_garden_security.py
+++ Python_Module_01/ex4/ft_garden_security.py
@@ -11,8 +11,46 @@ class Plant:
         self.set_height(height)
         self.set_age(age)
 
+    def set_height(self, height: float) -> None:
+        if height < 0:
+            print(f"{self._name}: Error, height can't be negative")
+            print("Height update rejected")
+        else:
+            self._height = height
+
+    def set_age(self, age: int) -> None:
+        if age < 0:
+            print(f"{self._name}: Error, age can't be negative")
+            print("Age update rejected")
+        else:
+            self._age = age
+
+    def get_height(self) -> float:
+        return self._height
+
+    def get_age(self) -> int:
+        return self._age
+
     def show(self) -> None:
-        print(f"{self.name}: {round(self.height, 1)}cm, {self.age} days old")
+        print(
+            f"{self._name}: {round(self._height, 1)}cm, "
+            f"{self._age} days old"
+            )
+
 
 if __name__ == "__main__":
-  print("=== Garden Security System ===")
+    print("=== Garden Security System ===")
+    rose = Plant("Rose", 15.0, 10)
+    print("Plant created: ", end="")
+    rose.show()
+
+    rose.set_height(25.0)
+    rose.set_age(30)
+    print("Height updated: 25cm")
+    print("Age updated: 30 days")
+
+    rose.set_height(-5.0)
+    rose.set_age(-1)
+
+    print("\nCurrent state: ", end="")
+    rose.show()
